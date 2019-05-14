1fb0638e43c4d0324161421a803f5f41b0994f92
zcs321 <50004918+zcs321@users.noreply.github.com>
2019-05-14 13:10:20 +0800

    我的学生查询

--- WebContent/WodeStu.jsp
+++ WebContent/WodeStu.jsp
@@ -5,8 +5,88 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="js/jquery-easyui-1.4.3/themes/icon.css" />
+<link rel="stylesheet" type="text/css" href="js/jquery-easyui-1.4.3/themes/metro-green/easyui.css"  />
+<script type="text/javascript" src="js/jquery-easyui-1.4.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery-easyui-1.4.3/jquery.easyui.min.js"></script>
+<script type="text/javascript" src="js/jquery-easyui-1.4.3/locale/easyui-lang-zh_CN.js"></script>
+<script type="text/javascript">
+	$(function(){
+		initWodeStu();
+	})
+	function initWodeStu(){
+		$('#ltab').datagrid({    
+		    url:"selectStu",
+		    method:"post",
+		    pagination:true,
+		    toolbar: '#tb'
+		});  
+	}
+	function formattercaozuo(value,row,index) {
+		return "<a href='javascript:void(0)' onclick='update("+index+")'>修改</a> <a href='javascript:void(0)' onclick='del("+index+")'>删除</a>   <a href='javascript:void(0)' onclick='chakan("+index+")'>查看</a>"
+	}
+	function update(index) {
+		var datas=$("#ltab").datagrid("getData");
+		var row=datas.rows[index];
+		$("#frmStu").form("load",row)
+		 $("#updateStu").dialog("open");
+	}
+
+</script>
 </head>
 <body>
-	这是我的学生
+	<table class="easyui-datagrid" id="ltab">   
+    <thead>   
+        <tr>   
+            <th data-options="field:'stu_id',title:'学生id'"></th>   
+            <th data-options="field:'stu_name',title:'学生姓名'"></th>   
+            <th data-options="field:'stu_sex',title:'性别'"></th>   
+            <th data-options="field:'stu_age',title:'年龄'"></th>   
+             <th data-options="field:'stu_phone',title:'电话'"></th>  
+            <th data-options="field:'stu_state',title:'个人状态'"></th>   
+            <th data-options="field:'stu_xueLi',title:'学历'"></th>   
+            <th data-options="field:'stu_quDao',title:'来源渠道'"></th>   
+            <th data-options="field:'stu_wangZhan',title:'来源网址'"></th>   
+            <th data-options="field:'stu_guanJian',title:'来源关键词'"></th>   
+            <th data-options="field:'stu_wx',title:'微信'"></th>   
+            <th data-options="field:'stu_baoBei',title:'是否报备'"></th>   
+            <th data-options="field:'stu_keCheng',title:'课程方向'"></th>   
+            <th data-options="field:'stu_youXiao',title:'是否有效'"></th>   
+            <th data-options="field:'stu_daFen',title:'打分'"></th>   
+            <th data-options="field:'stu_huiFang',title:'是否回访'"></th>   
+            <th data-options="field:'stu_visit',title:'是否上门'"></th>   
+            <th data-options="field:'stu_visitTime',title:'上门时间'"></th>   
+            <th data-options="field:'stu_baoBei',title:'是否报备'"></th>   
+            <th data-options="field:'stu_payTime',title:'首次回访时间'"></th>   
+            <th data-options="field:'stu_wuXiaoYuanYin',title:'无效原因'"></th>   
+            <th data-options="field:'stu_money',title:'是否缴费'"></th>   
+            <th data-options="field:'stu_moneyTime',title:'缴费时间'"></th>   
+            <th data-options="field:'stu_jinE',title:'金额'"></th>   
+            <th data-options="field:'stu_tui',title:'是否退费'"></th>   
+            <th data-options="field:'stu_class',title:'是否进班'"></th>   
+            <th data-options="field:'stu_classTime',title:'进班时间'"></th>   
+            <th data-options="field:'stu_classBeiZhu',title:'进班备注'"></th>   
+            <th data-options="field:'stu_tuiYin',title:'退费原因'"></th>   
+            <th data-options="field:'stu_dingJinE',title:'定金金额'"></th>   
+            <th data-options="field:'stu_dingTime',title:'定金时间'"></th>   
+            <th data-options="field:'stu_quYu',title:'所在区域'"></th>   
+            <th data-options="field:'stu_buMen',title:'来源部门'"></th>   
+            <th data-options="field:'stu_qq',title:'QQ'"></th>   
+            <th data-options="field:'stu_ziXun',title:'咨询姓名'"></th>   
+            <th data-options="field:'stu_creatorTime',title:'创建时间'"></th>   
+            <th data-options="field:'操作',title:'操作',formatter:formattercaozuo"></th>   
+        </tr>   
+    </thead>   
+</table>  
+<div id="tb">
+<form id="ff">   
+           <label for="email">Email:</label>   
+        <input class="easyui-validatebox" type="text" name="email" />       
+        <label for="email">Email:</label>   
+        <input class="easyui-validatebox" type="text" name="email" />   
+
+          <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-search'" onclick="initWodeStu()">搜索</a> 
+          <a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-add'" onclick="add()">添加书</a> 
+</form> 
 </body>
 </html>
--- src/com/ssj/entity/Student.java
+++ src/com/ssj/entity/Student.java
@@ -44,4 +44,271 @@ public class Student {
 	private String stu_keCheng;//课程方向
 	private String stu_daFen;//打分
 	private String stu_luRu;//录入人 
+	public Integer getStu_id() {
+		return stu_id;
+	}
+	public void setStu_id(Integer stu_id) {
+		this.stu_id = stu_id;
+	}
+	public String getStu_name() {
+		return stu_name;
+	}
+	public void setStu_name(String stu_name) {
+		this.stu_name = stu_name;
+	}
+	public Integer getStu_sex() {
+		return stu_sex;
+	}
+	public void setStu_sex(Integer stu_sex) {
+		this.stu_sex = stu_sex;
+	}
+	public Integer getStu_age() {
+		return stu_age;
+	}
+	public void setStu_age(Integer stu_age) {
+		this.stu_age = stu_age;
+	}
+	public String getStu_phone() {
+		return stu_phone;
+	}
+	public void setStu_phone(String stu_phone) {
+		this.stu_phone = stu_phone;
+	}
+	public String getStu_state() {
+		return stu_state;
+	}
+	public void setStu_state(String stu_state) {
+		this.stu_state = stu_state;
+	}
+	public String getStu_xueLi() {
+		return stu_xueLi;
+	}
+	public void setStu_xueLi(String stu_xueLi) {
+		this.stu_xueLi = stu_xueLi;
+	}
+	public String getStu_quDao() {
+		return stu_quDao;
+	}
+	public void setStu_quDao(String stu_quDao) {
+		this.stu_quDao = stu_quDao;
+	}
+	public String getStu_wangZhan() {
+		return stu_wangZhan;
+	}
+	public void setStu_wangZhan(String stu_wangZhan) {
+		this.stu_wangZhan = stu_wangZhan;
+	}
+	public String getStu_guanZhu() {
+		return stu_guanZhu;
+	}
+	public void setStu_guanZhu(String stu_guanZhu) {
+		this.stu_guanZhu = stu_guanZhu;
+	}
+	public String getStu_guanJian() {
+		return stu_guanJian;
+	}
+	public void setStu_guanJian(String stu_guanJian) {
+		this.stu_guanJian = stu_guanJian;
+	}
+	public String getStu_wx() {
+		return stu_wx;
+	}
+	public void setStu_wx(String stu_wx) {
+		this.stu_wx = stu_wx;
+	}
+	public String getStu_qq() {
+		return stu_qq;
+	}
+	public void setStu_qq(String stu_qq) {
+		this.stu_qq = stu_qq;
+	}
+	public String getStu_baoBei() {
+		return stu_baoBei;
+	}
+	public void setStu_baoBei(String stu_baoBei) {
+		this.stu_baoBei = stu_baoBei;
+	}
+	public String getStu_beiZhu() {
+		return stu_beiZhu;
+	}
+	public void setStu_beiZhu(String stu_beiZhu) {
+		this.stu_beiZhu = stu_beiZhu;
+	}
+	public String getStu_useryId() {
+		return stu_useryId;
+	}
+	public void setStu_useryId(String stu_useryId) {
+		this.stu_useryId = stu_useryId;
+	}
+	public Integer getStu_youXiao() {
+		return stu_youXiao;
+	}
+	public void setStu_youXiao(Integer stu_youXiao) {
+		this.stu_youXiao = stu_youXiao;
+	}
+	public String getStu_wuXiaoYuanYin() {
+		return stu_wuXiaoYuanYin;
+	}
+	public void setStu_wuXiaoYuanYin(String stu_wuXiaoYuanYin) {
+		this.stu_wuXiaoYuanYin = stu_wuXiaoYuanYin;
+	}
+	public Integer getStu_huiFang() {
+		return stu_huiFang;
+	}
+	public void setStu_huiFang(Integer stu_huiFang) {
+		this.stu_huiFang = stu_huiFang;
+	}
+	public String getStu_creatorTime() {
+		return stu_creatorTime;
+	}
+	public void setStu_creatorTime(String stu_creatorTime) {
+		this.stu_creatorTime = stu_creatorTime;
+	}
+	public Integer getStu_visit() {
+		return stu_visit;
+	}
+	public void setStu_visit(Integer stu_visit) {
+		this.stu_visit = stu_visit;
+	}
+	public String getStu_visitTime() {
+		return stu_visitTime;
+	}
+	public void setStu_visitTime(String stu_visitTime) {
+		this.stu_visitTime = stu_visitTime;
+	}
+	public String getStu_payTime() {
+		return stu_payTime;
+	}
+	public void setStu_payTime(String stu_payTime) {
+		this.stu_payTime = stu_payTime;
+	}
+	public Integer getStu_money() {
+		return stu_money;
+	}
+	public void setStu_money(Integer stu_money) {
+		this.stu_money = stu_money;
+	}
+	public String getStu_moneyTime() {
+		return stu_moneyTime;
+	}
+	public void setStu_moneyTime(String stu_moneyTime) {
+		this.stu_moneyTime = stu_moneyTime;
+	}
+	public Integer getStu_jinE() {
+		return stu_jinE;
+	}
+	public void setStu_jinE(Integer stu_jinE) {
+		this.stu_jinE = stu_jinE;
+	}
+	public Integer getStu_tui() {
+		return stu_tui;
+	}
+	public void setStu_tui(Integer stu_tui) {
+		this.stu_tui = stu_tui;
+	}
+	public String getStu_tuiYin() {
+		return stu_tuiYin;
+	}
+	public void setStu_tuiYin(String stu_tuiYin) {
+		this.stu_tuiYin = stu_tuiYin;
+	}
+	public Integer getStu_dingJinE() {
+		return stu_dingJinE;
+	}
+	public void setStu_dingJinE(Integer stu_dingJinE) {
+		this.stu_dingJinE = stu_dingJinE;
+	}
+	public String getStu_dingTime() {
+		return stu_dingTime;
+	}
+	public void setStu_dingTime(String stu_dingTime) {
+		this.stu_dingTime = stu_dingTime;
+	}
+	public Integer getStu_class() {
+		return stu_class;
+	}
+	public void setStu_class(Integer stu_class) {
+		this.stu_class = stu_class;
+	}
+	public String getStu_classTime() {
+		return stu_classTime;
+	}
+	public void setStu_classTime(String stu_classTime) {
+		this.stu_classTime = stu_classTime;
+	}
+	public String getStu_classBeiZhu() {
+		return stu_classBeiZhu;
+	}
+	public void setStu_classBeiZhu(String stu_classBeiZhu) {
+		this.stu_classBeiZhu = stu_classBeiZhu;
+	}
+	public String getStu_ziXun() {
+		return stu_ziXun;
+	}
+	public void setStu_ziXun(String stu_ziXun) {
+		this.stu_ziXun = stu_ziXun;
+	}
+	public String getStu_ziXunBeiZhu() {
+		return stu_ziXunBeiZhu;
+	}
+	public void setStu_ziXunBeiZhu(String stu_ziXunBeiZhu) {
+		this.stu_ziXunBeiZhu = stu_ziXunBeiZhu;
+	}
+	public String getStu_quYu() {
+		return stu_quYu;
+	}
+	public void setStu_quYu(String stu_quYu) {
+		this.stu_quYu = stu_quYu;
+	}
+	public String getStu_buMen() {
+		return stu_buMen;
+	}
+	public void setStu_buMen(String stu_buMen) {
+		this.stu_buMen = stu_buMen;
+	}
+	public String getStu_keCheng() {
+		return stu_keCheng;
+	}
+	public void setStu_keCheng(String stu_keCheng) {
+		this.stu_keCheng = stu_keCheng;
+	}
+	public String getStu_daFen() {
+		return stu_daFen;
+	}
+	public void setStu_daFen(String stu_daFen) {
+		this.stu_daFen = stu_daFen;
+	}
+	public String getStu_luRu() {
+		return stu_luRu;
+	}
+	public void setStu_luRu(String stu_luRu) {
+		this.stu_luRu = stu_luRu;
+	}
+	@Override
+	public String toString() {
+		return "Student [stu_id=" + stu_id + ", stu_name=" + stu_name
+				+ ", stu_sex=" + stu_sex + ", stu_age=" + stu_age
+				+ ", stu_phone=" + stu_phone + ", stu_state=" + stu_state
+				+ ", stu_xueLi=" + stu_xueLi + ", stu_quDao=" + stu_quDao
+				+ ", stu_wangZhan=" + stu_wangZhan + ", stu_guanZhu="
+				+ stu_guanZhu + ", stu_guanJian=" + stu_guanJian + ", stu_wx="
+				+ stu_wx + ", stu_qq=" + stu_qq + ", stu_baoBei=" + stu_baoBei
+				+ ", stu_beiZhu=" + stu_beiZhu + ", stu_useryId=" + stu_useryId
+				+ ", stu_youXiao=" + stu_youXiao + ", stu_wuXiaoYuanYin="
+				+ stu_wuXiaoYuanYin + ", stu_huiFang=" + stu_huiFang
+				+ ", stu_creatorTime=" + stu_creatorTime + ", stu_visit="
+				+ stu_visit + ", stu_visitTime=" + stu_visitTime
+				+ ", stu_payTime=" + stu_payTime + ", stu_money=" + stu_money
+				+ ", stu_moneyTime=" + stu_moneyTime + ", stu_jinE=" + stu_jinE
+				+ ", stu_tui=" + stu_tui + ", stu_tuiYin=" + stu_tuiYin
+				+ ", stu_dingJinE=" + stu_dingJinE + ", stu_dingTime="
+				+ stu_dingTime + ", stu_class=" + stu_class
+				+ ", stu_classTime=" + stu_classTime + ", stu_classBeiZhu="
+				+ stu_classBeiZhu + ", stu_ziXun=" + stu_ziXun
+				+ ", stu_ziXunBeiZhu=" + stu_ziXunBeiZhu + ", stu_quYu="
+				+ stu_quYu + ", stu_buMen=" + stu_buMen + ", stu_keCheng="
+				+ stu_keCheng + ", stu_daFen=" + stu_daFen + ", stu_luRu="
+				+ stu_luRu + "]";
+	}
+	
 }
